f64f6fca7df0e3d7ffb042720dc11e2d0a6a3c87
Dominik Cvetan <dominik.cvetan@gmail.com>
2024-12-01 15:23:49 +0100

    Zadatak 4

--- projekt/index.php
+++ projekt/index.php
@@ -34,5 +34,19 @@
             <figcaption>A beautiful weather scene</figcaption>
         </figure>
     </main>
+	<footer>
+        <p>Copyright © <?php echo date("Y");?> Dominik Cvetan</p>
+        <div class="social-links">
+            <a href="https://github.com/dcvetan" target="_blank">
+                <img src="img/github-icon.png" alt="GitHub">
+            </a>
+            <a href="https://facebook.com" target="_blank">
+                <img src="img/facebook-icon.png" alt="Facebook">
+            </a>
+            <a href="https://twitter.com" target="_blank">
+                <img src="img/twitter-icon.png" alt="Twitter">
+            </a>
+        </div>
+    </footer>
 </body>
 </html>
